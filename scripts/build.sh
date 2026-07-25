a0bc6feca8714f09bd1cad4cdb168e9966333a75
Piotr <piotrm@ekolaby.net>
2026-07-25 13:30:50 +0200

    Signing: persistent WorkField keystore for all build types; expose FileUtils::copyRecursively to QML

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,9 @@ SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"/..
 
 export APK_VERSION_CODE=${APK_VERSION_CODE:-600}
 export APP_VERSION_STR=${APP_VERSION_STR:-0.6.0}
+export STOREPASS=${STOREPASS:-workfield2026}
+export KEYNAME=${KEYNAME:-workfield}
+export KEYPASS=${KEYPASS:-workfield2026}
 
 triplet=${triplet:-arm64-android}
 
