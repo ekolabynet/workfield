b905d4286fe1e8dc3fae8d49029e668b11322b9c
Piotr <piotrm@ekolaby.net>
2026-07-27 08:24:36 +0200

    Version bump: 0.6.5 (versionCode 605)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ set -e
 
 SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"/..
 
-export APK_VERSION_CODE=${APK_VERSION_CODE:-604}
-export APP_VERSION_STR=${APP_VERSION_STR:-0.6.4}
+export APK_VERSION_CODE=${APK_VERSION_CODE:-605}
+export APP_VERSION_STR=${APP_VERSION_STR:-0.6.5}
 export STOREPASS=${STOREPASS:-workfield2026}
 export KEYNAME=${KEYNAME:-workfield}
 export KEYPASS=${KEYPASS:-workfield2026}
