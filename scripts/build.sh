483122529c02a7fdd240a5c71ab4c90e9a6e76e4
Piotr <piotrm@ekolaby.net>
2026-07-28 10:59:49 +0200

    Version bump: 0.7.3 (versionCode 703)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ set -e
 
 SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"/..
 
-export APK_VERSION_CODE=${APK_VERSION_CODE:-702}
-export APP_VERSION_STR=${APP_VERSION_STR:-0.7.2}
+export APK_VERSION_CODE=${APK_VERSION_CODE:-703}
+export APP_VERSION_STR=${APP_VERSION_STR:-0.7.3}
 export STOREPASS=${STOREPASS:-workfield2026}
 export KEYNAME=${KEYNAME:-workfield}
 export KEYPASS=${KEYPASS:-workfield2026}
