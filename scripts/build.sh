245713ec088ca304090332aaefe03a80f4f73a2b
Piotr <piotrm@ekolaby.net>
2026-07-29 06:56:58 +0200

    Version bump: 0.8.0 (versionCode 800)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ set -e
 
 SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"/..
 
-export APK_VERSION_CODE=${APK_VERSION_CODE:-709}
-export APP_VERSION_STR=${APP_VERSION_STR:-0.7.9}
+export APK_VERSION_CODE=${APK_VERSION_CODE:-800}
+export APP_VERSION_STR=${APP_VERSION_STR:-0.8.0}
 export STOREPASS=${STOREPASS:-workfield2026}
 export KEYNAME=${KEYNAME:-workfield}
 export KEYPASS=${KEYPASS:-workfield2026}
